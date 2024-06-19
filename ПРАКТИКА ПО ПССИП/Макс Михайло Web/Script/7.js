8743074c9a30673ba2b34dd2fbdc294c958f28da
MaximMihajlovskij <156321726+MaximMihajlovskij@users.noreply.github.com>
2024-06-19 19:58:37 +0300

    Сайт

---
+++ ПРАКТИКА ПО ПССИП/Макс Михайло Web/Script/7.js
@@ -0,0 +1,6 @@
+//Смена фона при нажатии на кнопку "Бесплатный размер"
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('first').addEventListener('click', () => {
+    document.body.style.backgroundColor = 'lightgray';
+  });
+});
